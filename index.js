d88dc4377940ffbd7d1faaae3c92c273dce70b2b
Eliezir Moreira <eliezir2004@gmail.com>
2022-08-08 12:27:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,234 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <link rel="stylesheet" href="./style.css">
-    <title>Document</title>
-</head>
-<body>
-    <div id="main">
-    <div class = "configs">
-        <div class="positions"><h1 id="L">L1</h1><h1 id="C">C1</h1></div>
-        <hr>
-        <div class="setas">
-        <i id="up" class="fa-solid fa-circle-arrow-up"></i>
-        <div class="divSetas"><i  id="left"class=" fa-solid fa-circle-arrow-left"></i><i  id="right" class="fa-solid fa-circle-arrow-right"></i></div>
-        <i  id="down" class="fa-solid fa-circle-arrow-down"></i>
-    <div class="radioBtn">
-        <p>Editar por:</p>
-        <div>
-         <div><input type="radio" id="td" name="direction" class="form-check-input" value="Td" checked>
-          <label for="td">Célula</label></div>
-          <div><input type="radio" id="tr" name="direction" class="form-check-input" value="Tr">
-          <label for="tr">Linha</label></div>
-          <div><input type="radio" id="col" name="direction" class="form-check-input" value="Col">
-          <label for="col">Coluna</label></div></div>
-    </div>        
-    </div>
-        <hr>
-        <div class="editTd">
-        <div class="texto">
-            <div class="textBtns">
-                <label class="input-text">
-                    <span>Texto</span>
-            <input type="text" id="textoTd" name="textoTd">
-            <div>
-            <button type="button" id="edit"><i class="fa-solid fa-pencil"></i></button>
-            <button type="button" id="delete"><i class="fa-solid fa-eraser"></i></button></div></div>
-        </div>
-        <hr> 
-        <div class="cor">
-            <div class="color-bar">
-            <label for="colorTd">Cor</label>
-            <input type="color" id="colorTd" name="colorTd" value="#6320ee";></div>
-            <div class="color-btn"><button type="button" id="paint">   <i class="fa-solid fa-fill-drip"></i></button></div>
-            </div></div>
-            <hr>
-            <div class="donwload">
-                <button type="button" id="donwload">BAIXAR</button>
-            </div>
-        </div>
-         
-        </div>
-        <div class="cores"></div>
-    </div></div>
-   <script src="index.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js" integrity="sha512-YcsIPGdhPK4P/uRW6/sruonlYj+Q7UHWeKfTAkBW+g83NKM+jMJFJ4iAPfSnVp7BKD4dKMHmVSvICUbE/V1sSw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>   
-</body>
-</html>
 
+
+/* Recebe um X e Y para criar a tabela */
+var tab_l = prompt("insira  um número de linhas");
+var tabela_linhas = parseInt(tab_l);
+var tab_c = prompt("insira  um número de colunas");
+var tabela_colunas = parseInt(tab_c);
+
+/* variáveis */
+/* var tabela_linhas = 8;
+var tabela_colunas = 7; */
+const divMain = document.getElementById("main");
+const table = document.createElement('table');
+var linhas = [];
+var colunas_linha = []
+var x = 0;
+var y = 0;
+var selectedRow = 0;
+var selectedCol = 0;
+
+var direction = "Td";
+
+/* Cria as tr, crias as tds, insere as tds nas tr, as linhas na table e a table no body */
+
+for(var y = 0; y < tabela_linhas ; y++){
+    var colunas = [];
+    linhas[y] = document.createElement('tr')
+for(var x = 0; x < tabela_colunas ; x++){
+    colunas[x] = document.createElement('td')
+    linhas[y].appendChild(colunas[x]);
+};
+table.appendChild(linhas[y]);
+x = 0;
+colunas_linha.push(colunas)}
+divMain.appendChild(table);
+
+
+/* funções para editar a tabela */
+
+function write(l,c,text){
+    colunas_linha[l][c].appendChild(document.createTextNode(text));
+}
+function erase(l,c){
+    colunas_linha[l][c].innerHTML = "";
+}
+
+function paint(l,c,color){
+    colunas_linha[l][c].style.backgroundColor=color;
+}
+
+function editRowOrColumn(func,direction){
+    for(x; x < direction;x-=-1){
+        func();   
+    }
+    x = 0;
+}
+
+/* event Listeners para mover a td selecionada */
+function clearFocus(){
+var celulaFoco = document.getElementsByClassName('hover')
+for (var i = celulaFoco.length-1; i > -1; i--) {
+    console.log(celulaFoco[i])
+    celulaFoco[i].classList.remove('hover');}}
+
+
+function changeFocus(dir,row,col){
+    for(y; y < dir;y++){
+        if(col == "All") colunas_linha[row][y].classList.toggle("hover");
+        else if(row ==="All")colunas_linha[y][col].classList.toggle("hover");
+        else colunas_linha[row][col].classList.toggle("hover");
+    }
+    y = 0;
+}
+
+
+function focusDirections(){
+    if(direction == "Col"){
+        changeFocus(tabela_linhas,"All",selectedCol);}
+    else if(direction =="Tr"){
+        changeFocus(tabela_colunas,selectedRow,"All");}
+    else if(direction == "Td"){
+        changeFocus(1,selectedRow,selectedCol);
+    }
+    }
+
+
+
+document.getElementById("up").addEventListener("click", function(){
+    /* colunas_linha[selectedRow][selectedCol].classList.remove("hover"); */
+    focusDirections();
+    selectedRow --
+    if (selectedRow < 0){selectedRow=tabela_linhas-1};
+    /* colunas_linha[selectedRow][selectedCol].classList.add("hover"); */
+    focusDirections();
+    document.getElementById("L").innerHTML="L"+(selectedRow+1);
+});
+document.getElementById("down").addEventListener("click", function(){
+   /*  colunas_linha[selectedRow][selectedCol].classList.remove("hover"); */
+   focusDirections();
+    selectedRow++;
+    if (selectedRow > tabela_linhas-1){selectedRow=0};
+    /* colunas_linha[selectedRow][selectedCol].classList.add("hover"); */
+    focusDirections();
+    document.getElementById("L").innerHTML="L"+(selectedRow+1);
+});
+document.getElementById("left").addEventListener("click", function(){
+    /* colunas_linha[selectedRow][selectedCol].classList.remove("hover"); */
+    focusDirections();
+    selectedCol --;
+    if (selectedCol < 0 ){selectedCol=tabela_colunas-1};
+    /* colunas_linha[selectedRow][selectedCol].classList.add("hover"); */
+    focusDirections();
+    document.getElementById("C").innerHTML="C"+(selectedCol+1);
+});
+document.getElementById("right").addEventListener("click", function(){
+    /* colunas_linha[selectedRow][selectedCol].classList.remove("hover"); */
+    focusDirections();
+    selectedCol ++;
+    if (selectedCol > tabela_colunas-1){selectedCol=0}
+    /* colunas_linha[selectedRow][selectedCol].classList.add("hover"); */
+    focusDirections();
+    document.getElementById("C").innerHTML="C"+(selectedCol+1);
+});
+
+/* event listeners edit texto delete texto */
+
+function edit(func,type){
+    if(direction == "Col"){
+    editRowOrColumn(function(){func(x,selectedCol,type)},tabela_linhas)}
+    else if(direction =="Tr"){
+        editRowOrColumn(function(){func(selectedRow,x,type)},tabela_colunas)}
+    else if(direction == "Td"){
+        editRowOrColumn(function(){func(selectedRow,selectedCol,type)},1)
+    }
+    }
+
+
+
+document.getElementById("edit").addEventListener("click", function(){
+    var texto = document.getElementById("textoTd").value;
+    edit(write,texto);
+})
+
+document.getElementById("delete").addEventListener("click", function(){
+    edit(erase,"delete")
+})
+
+document.getElementById("paint").addEventListener("click", function(){
+    var cor = document.getElementById("colorTd").value;
+    edit(paint,cor)
+})
+
+/* event listeners para direção da edição(celula/linha/coluna) */
+
+var dir = document.querySelectorAll(".form-check-input");
+function disabled(dir1,dir2){
+    document.getElementById(dir1).classList.add("disabled");
+    document.getElementById(dir2).classList.add("disabled");
+}
+function enabled(dir1,dir2){
+    document.getElementById(dir1).classList.remove("disabled");
+    document.getElementById(dir2).classList.remove("disabled");
+}
+
+
+dir.forEach(function(e){
+    e.addEventListener("click", function(){
+        direction = this.value;
+/* desabilitando controles, ao selecionar coluna só poderá andar verticalmente ... */
+    if(direction == "Col"){ 
+        document.getElementById("L").classList.add("hidden");
+        document.getElementById("C").classList.remove("hidden");
+       disabled("up","down");
+       enabled("left","right")}
+    else if(direction == "Tr"){
+        document.getElementById("C").classList.add("hidden");
+        document.getElementById("L").classList.remove("hidden");
+        enabled("up","down");
+        disabled("left","right");}
+    else if(direction == "Td"){  
+        document.getElementById("L").classList.remove("hidden");
+        document.getElementById("C").classList.remove("hidden");
+        enabled("left","right");
+        enabled("up","down");}
+        clearFocus();
+        focusDirections();
+      });
+
+    });
+
+    const handleFocus = ({target}) => {
+        if(target.type==="button"){
+          target = inputs[1]}
+        const span = target.parentElement.firstElementChild;
+        span.classList.add('span-active');  
+      }
+      
+      const handleFocusOut = ({target}) => {
+        if(target.type==="button"){
+          target = inputs[1]}
+        else if(target.value ===''){
+          const span = target.parentElement.firstElementChild;
+          span.classList.remove('span-active');}
+            
+      }
+
+const input_texto = document.getElementById("textoTd")
+input_texto.addEventListener('focus',handleFocus);
+input_texto.addEventListener('focusout',handleFocusOut);
+
+
+;
+var opt = {
+  margin:       1,
+  filename:     'table.pdf',
+  image:        { type: 'jpeg', quality: 0.98 },
+  html2canvas:  { scale: 2 },
+  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+};
+
+document.getElementById("donwload").addEventListener("click",function(){
+    html2pdf().set(opt).from(table).save()
+})
+
+
+
+/* editRowOrColumn(function(){paint(2,2,"blue")},tabela_colunas) */
+
+
+/* editRowOrColumn(function(){write(x,selectedCol,"boa tarde")},tabela_linhas)
+editRowOrColumn(function(){paint(selectedCol,x,"purple")},tabela_colunas) */
+
+/* var texto = document.getElementById("textoTd").value;
+edit(write,texto) */
