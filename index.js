996c3cefba8dd721157d2ebf27873ee8154ba55f
Eliezir Moreira <eliezir2004@gmail.com>
2022-08-07 22:05:28 -0300

    JS pronto

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 
 
 /* Recebe um X e Y para criar a tabela */
-var tab_l = prompt("insira  um número de linhas");
+/* var tab_l = prompt("insira  um número de linhas");
 var tabela_linhas = parseInt(tab_l);
 var tab_c = prompt("insira  um número de colunas");
 var tabela_colunas = parseInt(tab_c);
-
+ */
 /* variáveis */
-/* var tabela_linhas = 8;
-var tabela_colunas = 7; */
-const body = document.body;
+var tabela_linhas = 8;
+var tabela_colunas = 7;
+const divMain = document.getElementById("main");
 const table = document.createElement('table');
 var linhas = [];
 var colunas_linha = []
 var x = 0;
-
+var y = 0;
 var selectedRow = 0;
 var selectedCol = 0;
+
+var direction = "Td";
 
 /* Cria as tr, crias as tds, insere as tds nas tr, as linhas na table e a table no body */
 
@@ -30,7 +32,7 @@ for(var x = 0; x < tabela_colunas ; x++){
 table.appendChild(linhas[y]);
 x = 0;
 colunas_linha.push(colunas)}
-document.body.appendChild(table);
+divMain.appendChild(table);
 
 
 /* funções para editar a tabela */
@@ -54,51 +56,147 @@ function editRowOrColumn(func,direction){
 }
 
 /* event Listeners para mover a td selecionada */
+function clearFocus(){
+var celulaFoco = document.getElementsByClassName('hover')
+for (var i = celulaFoco.length-1; i > -1; i--) {
+    console.log(celulaFoco[i])
+    celulaFoco[i].classList.remove('hover');}}
+
+
+
+function changeFocus(dir,row,col){
+    for(y; y < dir;y++){
+        if(col == "All") colunas_linha[row][y].classList.toggle("hover");
+        else if(row ==="All")colunas_linha[y][col].classList.toggle("hover");
+        else colunas_linha[row][col].classList.toggle("hover");
+    }
+    y = 0;
+}
+
+
+function focusDirections(){
+    if(direction == "Col"){
+        changeFocus(tabela_linhas,"All",selectedCol);}
+    else if(direction =="Tr"){
+        changeFocus(tabela_colunas,selectedRow,"All");}
+    else if(direction == "Td"){
+        changeFocus(1,selectedRow,selectedCol);
+    }
+    }
+
+
 
 document.getElementById("up").addEventListener("click", function(){
-    colunas_linha[selectedRow][selectedCol].classList.remove("hover");
+    /* colunas_linha[selectedRow][selectedCol].classList.remove("hover"); */
+    focusDirections();
     selectedRow --
     if (selectedRow < 0){selectedRow=tabela_linhas-1};
-    colunas_linha[selectedRow][selectedCol].classList.add("hover");
+    /* colunas_linha[selectedRow][selectedCol].classList.add("hover"); */
+    focusDirections();
     document.getElementById("L").innerHTML="L"+(selectedRow+1);
 });
 document.getElementById("down").addEventListener("click", function(){
-    colunas_linha[selectedRow][selectedCol].classList.remove("hover");
+   /*  colunas_linha[selectedRow][selectedCol].classList.remove("hover"); */
+   focusDirections();
     selectedRow++;
     if (selectedRow > tabela_linhas-1){selectedRow=0};
-    colunas_linha[selectedRow][selectedCol].classList.add("hover");
+    /* colunas_linha[selectedRow][selectedCol].classList.add("hover"); */
+    focusDirections();
     document.getElementById("L").innerHTML="L"+(selectedRow+1);
 });
 document.getElementById("left").addEventListener("click", function(){
-    colunas_linha[selectedRow][selectedCol].classList.remove("hover");
+    /* colunas_linha[selectedRow][selectedCol].classList.remove("hover"); */
+    focusDirections();
     selectedCol --;
     if (selectedCol < 0 ){selectedCol=tabela_colunas-1};
-    colunas_linha[selectedRow][selectedCol].classList.add("hover");
+    /* colunas_linha[selectedRow][selectedCol].classList.add("hover"); */
+    focusDirections();
     document.getElementById("C").innerHTML="C"+(selectedCol+1);
 });
 document.getElementById("right").addEventListener("click", function(){
-    colunas_linha[selectedRow][selectedCol].classList.remove("hover");
+    /* colunas_linha[selectedRow][selectedCol].classList.remove("hover"); */
+    focusDirections();
     selectedCol ++;
     if (selectedCol > tabela_colunas-1){selectedCol=0}
-    colunas_linha[selectedRow][selectedCol].classList.add("hover");
+    /* colunas_linha[selectedRow][selectedCol].classList.add("hover"); */
+    focusDirections();
     document.getElementById("C").innerHTML="C"+(selectedCol+1);
 });
 
 /* event listeners edit texto delete texto */
 
+function edit(func,type){
+    if(direction == "Col"){
+    editRowOrColumn(function(){func(x,selectedCol,type)},tabela_linhas)}
+    else if(direction =="Tr"){
+        editRowOrColumn(function(){func(selectedRow,x,type)},tabela_colunas)}
+    else if(direction == "Td"){
+        editRowOrColumn(function(){func(selectedRow,selectedCol,type)},1)
+    }
+    }
+
+
+
 document.getElementById("edit").addEventListener("click", function(){
     var texto = document.getElementById("textoTd").value;
-    write(selectedRow,selectedCol,texto)
+    edit(write,texto);
 })
 
 document.getElementById("delete").addEventListener("click", function(){
-    erase(selectedRow,selectedCol)
+    edit(erase,"delete")
 })
 
 document.getElementById("paint").addEventListener("click", function(){
     var cor = document.getElementById("colorTd").value;
-    paint(selectedRow,selectedCol,cor);
+    edit(paint,cor)
 })
 
+/* event listeners para direção da edição(celula/linha/coluna) */
+
+var dir = document.querySelectorAll(".form-check-input");
+function disabled(dir1,dir2){
+    document.getElementById(dir1).classList.add("disabled");
+    document.getElementById(dir2).classList.add("disabled");
+}
+function enabled(dir1,dir2){
+    document.getElementById(dir1).classList.remove("disabled");
+    document.getElementById(dir2).classList.remove("disabled");
+}
 
 
+dir.forEach(function(e){
+    e.addEventListener("click", function(){
+        direction = this.value;
+/* desabilitando controles, ao selecionar coluna só poderá andar verticalmente ... */
+    if(direction == "Col"){ 
+        document.getElementById("L").classList.add("hidden");
+        document.getElementById("C").classList.remove("hidden");
+       disabled("up","down");
+       enabled("left","right")}
+    else if(direction == "Tr"){
+        document.getElementById("C").classList.add("hidden");
+        document.getElementById("L").classList.remove("hidden");
+        enabled("up","down");
+        disabled("left","right");}
+    else if(direction == "Td"){  
+        document.getElementById("L").classList.remove("hidden");
+        document.getElementById("C").classList.remove("hidden");
+        enabled("left","right");
+        enabled("up","down");}
+        clearFocus();
+        focusDirections();
+      });
+
+    });
+
+
+
+
+/* editRowOrColumn(function(){paint(2,2,"blue")},tabela_colunas) */
+
+
+/* editRowOrColumn(function(){write(x,selectedCol,"boa tarde")},tabela_linhas)
+editRowOrColumn(function(){paint(selectedCol,x,"purple")},tabela_colunas) */
+
+/* var texto = document.getElementById("textoTd").value;
+edit(write,texto) */
